a2387931f6dcd1d3ed85ba77c343fe32d76089e1
helen <helen8335@gmail.com>
2025-09-16 17:27:25 -0500

    Initial commit

---
+++ src/main/java/co/edu/uniquindio/BarakaLashes/BarakaLashesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.BarakaLashes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarakaLashesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarakaLashesApplication.class, args);
+	}
+
+}
