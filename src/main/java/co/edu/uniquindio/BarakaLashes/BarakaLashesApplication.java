5898685de320d6d32d4788675b20aa8da46f9c5a
Esteban Maya <juanestebanmayasanchez@gmail.com>
2025-11-02 18:23:59 -0500

    Front Recordatorios Citas

--- src/main/java/co/edu/uniquindio/BarakaLashes/BarakaLashesApplication.java
+++ src/main/java/co/edu/uniquindio/BarakaLashes/BarakaLashesApplication.java
@@ -2,8 +2,10 @@ package co.edu.uniquindio.BarakaLashes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BarakaLashesApplication {
 
 	public static void main(String[] args) {
